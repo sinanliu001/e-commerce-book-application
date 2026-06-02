b552d543c38631910f6e98bd5c804af7f8d794b2
simonliu <simonliuoffer001@gmail.com>
2026-06-01 21:38:35 -0400

    feat: shopping cart UI

--- BulkyBook.DataAccess/Repository/Repository.cs
+++ BulkyBook.DataAccess/Repository/Repository.cs
@@ -40,9 +40,12 @@ namespace BulkyBook.DataAccess.Repository
             }
             return query.FirstOrDefault();
         }
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter,  string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if (filter != null) {
+                query = query.Where(filter);
+            }
             if (!string.IsNullOrEmpty(includeProperties))
             {
                 foreach (var includeProp in includeProperties
---
+++ BulkyBook.Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models.ViewModels {
+	public class ShoppingCartVM {
+		public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
+        
+		public double OrderTotal{ get; set; }
+    }
+}
