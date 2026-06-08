f4436aed9084aab5e33358e3f1bc0c95d47a12a7
simonliu <simonliuoffer001@gmail.com>
2026-06-07 20:04:00 -0400

    feat: update summary UI

--- BulkyBook.Models/OrderHeader.cs
+++ BulkyBook.Models/OrderHeader.cs
@@ -32,14 +32,17 @@ namespace BulkyBook.Models {
         public string? PaymentIntentId { get; set; }
 
         [Required]
+        [Display(Name = "Phone")]
         public string PhoneNumber { get; set; }
         [Required]
+        [Display(Name = "Street Address")]
         public string StreetAddress { get; set; }
         [Required]
         public string City { get; set; }
         [Required]
         public string State { get; set; }
         [Required]
+        [Display(Name = "Postal Code")]
         public string PostalCode { get; set; }
         [Required]
         public string Name { get; set; }
